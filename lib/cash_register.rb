f8ee7eeb0b5a8956009abd28d536eb058df4aa8e
Kayla Cross <kac288@georgetown.edu>
2018-03-25 21:12:23 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,10 +1,11 @@
 
 class CashRegister
 
-  attr_accessor :total
+  attr_accessor :total, :discount
 
-  def initialize(*args)
+  def initialize(discount=nil)
     self.total = 0
+    discount =
   end
 
 end
