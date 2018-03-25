ab98e2fa78e379cb90b08fe372aae1637ca60056
Kayla Cross <kac288@georgetown.edu>
2018-03-25 21:22:21 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -17,8 +17,8 @@ class CashRegister
     if discount
       self.total *= (1 - @discount/100.0)
       return "After the discount, the total comes to $#{@total.to_i}."
-    else
-      
+    else return "There is no discount to apply." 
+
   end
 
 end
