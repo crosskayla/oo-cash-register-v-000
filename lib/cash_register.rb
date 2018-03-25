7db0689ca3e28db7ec494bea2e8ae326ab8ee1f1
Kayla Cross <kac288@georgetown.edu>
2018-03-25 21:15:16 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -3,13 +3,17 @@ class CashRegister
 
   attr_accessor :total, :discount
 
-  def initialize(discount=nil)
+  def initialize(discount=0)
     self.total = 0
     self.discount = discount
   end
 
   def add_item(title, price, quantity=1)
     self.total += price*quantity
+  end
+
+  def apply_discount
+
   end
 
 end
